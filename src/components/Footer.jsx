bea6713e7a5111aba23d09a4d8af70305446a4d6
unique2721 <uniquehabeshawi@gmail.com>
2024-10-04 14:51:46 -0700

    #

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,16 @@
 
 const Footer = () => {
   return (
-    <div className="flex justify-center items-center flex-col">
+    <div className="mt-20 mb-10">
       <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide"><span className=" bg-gradient-to-t from-orange-500 to-red-800 text-transparent bg-clip-text">{" "}Links</span> </h1>
+      <div className="flex justify-between items-center pt-10">
+        <h3 className="text-3xl tracking-wide">copyright &copy; 2024 Gebremariam Kidane</h3>
+        <div className="text-3xl">
+            <a className="mx-10" href="">Github</a>
+            <a className="mx-10" href="">Figma</a>
+            <a className="mx-10" href="">Vercel</a>
+        </div>
+      </div>
     </div>
   )
 }
