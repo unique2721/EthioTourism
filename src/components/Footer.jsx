10c5e5f3642f559c215d313c73cca5a57b49c28f
unique2721 <uniquehabeshawi@gmail.com>
2024-10-04 14:38:44 -0700

    #

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
   return (
-    <div>
+    <div className="flex justify-center items-center flex-col">
       <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">Links </h1>
     </div>
   )
