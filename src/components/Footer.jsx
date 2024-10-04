5addd85218a1dffd0dc9b6a2ad3d78bd12ef69f2
unique2721 <uniquehabeshawi@gmail.com>
2024-10-04 14:39:23 -0700

    #

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 const Footer = () => {
   return (
     <div className="flex justify-center items-center flex-col">
-      <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">Links </h1>
+      <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide"><span className=" bg-gradient-to-t from-orange-500 to-red-800 text-transparent bg-clip-text">{" "}Links</span> </h1>
     </div>
   )
 }
