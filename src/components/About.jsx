79654e771e4c84c8e7931e6ab8512f52472d5761
unique2721 <uniquehabeshawi@gmail.com>
2024-10-04 15:56:08 -0700

    #

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
     <Navbar/>
     <div className="flex flex-col items-center mt-6 lg:mt-20">
       <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">About <span className=" bg-gradient-to-t from-orange-500 to-red-800 text-transparent bg-clip-text">{" "}Us</span></h1>
-      <p className="mt-10 text-lg text-center text-neutral-300 max-w-4xl">We specialize in providing unforgetable expriences in Ethiopia, from breathtaking landscapes to rich cultural heritage.</p>
+      <p className="mt-10 mx-4 text-lg text-center text-neutral-300 max-w-4xl">We specialize in providing unforgetable expriences in Ethiopia, from breathtaking landscapes to rich cultural heritage.</p>
       <div className="flex justify-center my-10">
         <Link to={'/'} className=" py-3 px-4 mx-3 rounded-md border hover:text-neutral-400">Back to Home</Link>
         </div>
