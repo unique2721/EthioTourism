50ba611d0cabc3873ecacae1e6fc6d6b90c11589
unique2721 <uniquehabeshawi@gmail.com>
2024-10-04 15:31:13 -0700

    #

--- src/router.jsx
+++ src/router.jsx
@@ -2,9 +2,12 @@ import App from './App.jsx'
 import Hero from './components/Hero.jsx'
 import About from './components/About.jsx'
 /* react routing */
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom'
+
 const router = createBrowserRouter([
  {path: '/', element: <App/>},
  {path: '/home', element: <Hero/>},
  {path: '/about',  element: <About/>}
 ])
+
+export default router
