2999f84a7f6b40f9e933edb2064fdc31fd1460ce
unique2721 <uniquehabeshawi@gmail.com>
2024-10-04 14:23:59 -0700

    #

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,12 @@
 
 const About = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center mt-6 lg:mt-20">
       <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">About <span className=" bg-gradient-to-t from-orange-500 to-red-800 text-transparent bg-clip-text">{" "}Us</span></h1>
+      <p className="mt-10 text-lg text-center text-neutral-300 max-w-4xl">We specialize in providing unforgetable expriences in Ethiopia, from breathtaking landscapes to rich cultural heritage.</p>
+      <div className="flex justify-center my-10">
+        <a href="#about" className=" bg-gradient-to-t from-orange-500 t0-orange-800 py-3 px-4 mx-3 rounded-md">Back to Home</a>
+        </div>
     </div>
   )
 }
