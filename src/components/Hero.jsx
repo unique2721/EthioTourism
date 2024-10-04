c5901b806e2c5f55180b276673a6065f5d00a000
unique2721 <uniquehabeshawi@gmail.com>
2024-10-04 13:50:19 -0700

    intial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+/* images */
+import abayyy from '../assets/abayyy.png'
+import axum from '../assets/axum.png'
+
+const Hero = () => {
+  return (
+    <div className="flex flex-col items-center mt-6 lg:mt-20">
+      <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">Explore <span className=" bg-gradient-to-t from-orange-500 to-red-800 text-transparent bg-clip-text">{" "}Ethiopia</span></h1>
+      <p className="mt-10 text-lg text-center text-neutral-300 max-w-4xl">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius minus aperiam culpa id, perspiciatis, et quasi asperiores corporis delectus repellendus voluptate ad. Impedit dicta ullam omnis, voluptate explicabo sed corrupti? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque est velit, ut sint placeat, fugiat iste voluptatum consequuntur debitis optio, animi error iure veniam illum cum voluptate suscipit corrupti blanditiis?</p>
+      <div className="flex justify-center my-10">
+        <a href="#about" className=" bg-gradient-to-t from-orange-500 t0-orange-800 py-3 px-4 mx-3 rounded-md">Learn More</a>
+      </div>
+      <div className="flex justify-center items-center mt-10 w-1/2">
+        <img className=" rounded-lg border w-full border-x-orange-700 shadow-orange-400 mx-2 my-4" src={abayyy} alt="abay" />
+        <img className=" rounded-lg border w-full border-x-orange-700 shadow-orange-400 mx-2 my-4" src={axum} alt="axum" />
+      </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+/* icons */
+import { Menu,X } from 'lucide-react'
+import { useState } from 'react'
+/* images */
+import logo from '../assets/logo.png'
+
+const Navbar = () => {
+const [mobileDrawOpen,setMobileDrawOpen] = useState(false);
+
+const toggleMenu = () => {
+    setMobileDrawOpen(!mobileDrawOpen)
+}
+  return (
+    <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
+        <div className="container px-4 mx-auto relative text">
+            <div className="flex justify-between items-center">
+                <div className="flex items-center flex-shrink-0">
+                    <img className='h-10 w-10 mr-2 rounded-full' src={logo} alt="logo" />
+                    <span className='text-xl tracking-tight'>EthioTourism</span>
+                </div>
+                <ul className='hidden lg:flex ml-14 space-x-24'>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                </ul>
+                <div className=' lg:hidden md:flex flex-col justify-end'>
+                    <button onClick={toggleMenu}>
+                        {mobileDrawOpen? <X/>:<Menu/>}
+                    </button>
+                </div>
+            </div>
+            {
+                mobileDrawOpen && (
+                    <div className=" fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden">
+                        <ul>
+                        <li className='py-4 '><a href="#">Home</a></li>
+                        <li className='py-4 '><a href="#about">About</a></li>
+                        </ul>
+                        
+                    </div>
+                )
+            }
+          </div>
+    </nav>
+  )
+}
+
+export default Navbar
