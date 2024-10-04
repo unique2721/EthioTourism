4eb17765d896a8af0966b34ee21e77940829c285
unique2721 <uniquehabeshawi@gmail.com>
2024-10-04 14:58:31 -0700

    #

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,10 @@ const Footer = () => {
       <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide"><span className=" bg-gradient-to-t from-orange-500 to-red-800 text-transparent bg-clip-text">{" "}Links</span> </h1>
       <div className="flex justify-between items-center pt-10">
         <h3 className="text-3xl tracking-wide">copyright &copy; 2024 Gebremariam Kidane</h3>
-        <div className="text-3xl">
-            <a className="mx-10" href="">Github</a>
-            <a className="mx-10" href="">Figma</a>
-            <a className="mx-10" href="">Vercel</a>
+        <div className="text-2xl text-neutral-300">
+            <a className="mx-10 hover:text-neutral-400" href="">Github</a>
+            <a className="mx-10 hover:text-neutral-400" href="">Figma</a>
+            <a className="mx-10 hover:text-neutral-400" href="">Vercel</a>
         </div>
       </div>
     </div>
