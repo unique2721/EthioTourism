b0e072c1953d81ba7da1b0dec52afcaa734bc462
unique2721 <uniquehabeshawi@gmail.com>
2024-10-05 09:15:52 -0700

    #

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,9 +9,9 @@ const Footer = () => {
       <div className="flex items-center pt-10 mx-4 sm:justify-center lg:justify-between">
         <h3 className="text-3xl tracking-wide hidden lg:flex">&copy; 2024 Gebremariam Kidane</h3>
         <div className="flex items-center justify-center text-2xl text-neutral-300">
-            <a className=" flex items-center justify-center space-x-1 mx-10 hover:text-neutral-400" href="https://github.com/unique2721/EthioTourism" target="_blank"><p>Github</p> <img className=' rounded-full w-7 h-7' src={gitlogo} alt="git" /> </a>
-            <a className=" flex items-center justify-center space-x-1 mx-10 hover:text-neutral-400" href=""><p>Figma</p> <img className=' rounded-full w-7 h-7' src={figmalogo} alt="figmalogo" /> </a>
-            <a href="https://ethio-tourism.vercel.app/" target="_blank" className=" flex items-center justify-center space-x-1 mx-10 hover:text-neutral-400"><p>Vercel</p> <img className=' rounded-full w-7 h-7' src={vercellogo} alt="vercel" /></a>
+            <a className=" flex items-center justify-center space-x-1 mx-10 hover:text-neutral-400" href="https://github.com/unique2721/EthioTourism" target="_blank"><p>Github</p> <img className='hidden sm:flex rounded-full w-7 h-7' src={gitlogo} alt="git" /> </a>
+            <a className=" flex items-center justify-center space-x-1 mx-10 hover:text-neutral-400" href=""><p>Figma</p> <img className='hidden sm:flex rounded-full w-7 h-7' src={figmalogo} alt="figmalogo" /> </a>
+            <a href="https://ethio-tourism.vercel.app/" target="_blank" className=" flex items-center justify-center space-x-1 mx-10 hover:text-neutral-400"><p>Vercel</p> <img className='hidden sm:flex rounded-full w-7 h-7' src={vercellogo} alt="vercel" /></a>
         </div>
       </div>
     </div>
