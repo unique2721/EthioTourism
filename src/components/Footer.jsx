8489f3b5d8ab749befdf55cb8ae5008975d9718c
unique2721 <uniquehabeshawi@gmail.com>
2024-10-07 12:00:31 -0700

    #

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,6 @@ import vercellogo from '../assets/vercellogo.png'
 const Footer = () => {
   return (
     <div className="mt-20 mx-10 border-t py-10 border-x-neutral-700 overflow-hidden">
-      <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide"><span className=" bg-gradient-to-t from-orange-500 to-red-800 text-transparent bg-clip-text">{" "}Links</span> </h1>
       <div className="flex items-center pt-10 mx-4 sm:justify-center lg:justify-between">
         <h3 className="text-3xl tracking-wide hidden lg:flex">&copy; 2024 Gebremariam Kidane</h3>
         <div className="flex flex-wrap items-center justify-center text-2xl text-neutral-300">
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -105,7 +105,7 @@ const Hero = () => {
           <div className='flex flex-col justify-center items-center gap-4 p-4 h-2/3'>
             <p className='text-xl font-semibold'>{d.name}</p>
             <p className='text-center'>{d.description}</p>
-            <button className='bg-indigo-500 text-white text-lg px-6 py-1 rounded-xl'>Read More</button>
+            <button className='bg-indigo-500 bg-gradient-to-t text-white text-lg px-6 py-1 rounded-xl'>Read More</button>
           </div>
           </div>
          ))
