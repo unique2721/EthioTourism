ba75f540aceecf073a37255cbe68d80c82638f37
unique2721 <uniquehabeshawi@gmail.com>
2024-10-07 11:15:21 -0700

    #

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,32 @@ const Hero = () => {
     infinite: true,
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
   };
 
    const data = [
